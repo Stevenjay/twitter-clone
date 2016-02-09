45c39b3688cf39d3e9f01c728a714bfd09f0a04e
Steven Jay <stevenjasionowicz@gmail.com>
2016-02-09 14:03:00 +1300

    create register controller and view

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,10 @@
+@extends('master')
+
+@section('title', 'Register')
+@section('meta-description', 'Sign up for a twitter account')
+
+@section('content')
+
+<h1>Register new account</h1>
+
+@endsection
