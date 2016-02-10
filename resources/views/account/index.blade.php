bec78d1277931f52b5f0219f7cad52c615597608
Steven Jay <stevenjasionowicz@gmail.com>
2016-02-10 14:23:18 +1300

    add tweets to account page

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -7,4 +7,32 @@
 
 <h1>Hi {{ \Auth::user()->name }}</h1>
 
+<h2>Your Stats:</h2>
+<ul>
+	<li>Total Tweets: {{ $totalTweets }}</li>
+</ul>
+
+<h2>Write a tweet</h2>
+
+<form action="/account/new-tweet" method="post">
+
+	{!! csrf_field() !!}
+
+	<div>
+		<label for="content">Tweet: </label>
+		<textarea name="content" id="content" cols="30" rows="10">{{ old('content') }}</textarea>
+	</div>
+
+	<input type="submit" value="Tweet">
+
+</form>
+
+@if(count($errors))
+	<ul>
+		@foreach($errors->all() as $error)
+		<li>{{ $error }}</li>
+		@endforeach
+	</ul>
+@endif
+
 @endsection
